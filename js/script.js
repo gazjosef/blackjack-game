c65ab22fcbe3d72f0b96edb2cd4e98161723ca6e
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-04-05 00:49:13 +1100

    Working version of the game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
+// Arrays
 const cards = [];
 let playerCard = [];
 let dealerCard = [];
-let cardCount = 0;
-
-let mydollars = 1000;
 const suits = ["spades", "hearts", "clubs", "diams"];
 const numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+// Game Status
+let cardCount = 0;
+let mydollars = 1000;
+let endplay = false;
+
+// Dom Elements
+const message = document.getElementById("message");
 const output = document.getElementById("output");
 const dealerHolder = document.getElementById("dealerHolder");
 const playerHolder = document.getElementById("playerHolder");
+const pValue = document.getElementById("pValue");
+const dValue = document.getElementById("dValue");
+const dollarValue = document.getElementById("dollars");
+document.getElementById("mybet").onchange = function() {
+  if (this.value < 0) {
+    this.value = 0;
+  }
+  if (this.value > mydollars) {
+    this.value = mydollars;
+  }
+  message.innerHTML = "Bet changed to $" + this.value;
+};
 
+// Build deck of cards
 for (s in suits) {
   let suit = suits[s][0].toUpperCase();
   const bgcolor = suit == "S" || suit == "C" ? "black" : "red";
@@ -35,21 +54,50 @@ function Start() {
 }
 
 function dealNew() {
+  dValue.innerHTML = "?";
   playerCard = [];
   dealerCard = [];
   dealerHolder.innerHTML = "";
   playerHolder.innerHTML = "";
+
+  const betvalue = document.getElementById("mybet").value;
+  mydollars = mydollars - betvalue;
+  document.getElementById("dollars").innerHTML = mydollars;
+  document.getElementById("myactions").style.display = "block";
+  message.innerHTML =
+    "Get up to 21 and beat the dealer to win.<br>Current bet is $" + betvalue;
+  document.getElementById("mybet").disabled = true;
+  document.getElementById("maxbet").disabled = true;
+  deal();
+  document.getElementById("btndeal").style.display = "none";
+}
+
+function redeal() {
+  cardCount++;
+  if (cardCount > 40) {
+    console.log("New Deck");
+    shuffleDeck(cards);
+    cardCount = 0;
+    message.innerHTML = "New Shuffle";
+  }
+}
+
+function deal() {
+  console.log(cards);
+
+  // Card count reshuffle
   for (x = 0; x < 2; x++) {
     dealerCard.push(cards[cardCount]);
     dealerHolder.innerHTML += cardOutput(cardCount, x);
     if (x === 0) {
       dealerHolder.innerHTML += '<div id="cover" style="left: 100px"></div>';
     }
-    cardCount++;
+    redeal();
     playerCard.push(cards[cardCount]);
     playerHolder.innerHTML += cardOutput(cardCount, x);
-    cardCount++;
+    redeal();
   }
+  pValue.innerHTML = checktotal(playerCard);
   console.log(dealerCard);
   console.log(playerCard);
 }
@@ -67,6 +115,127 @@ function cardOutput(n, x) {
     cards[n].cardnum +
     "<br></div> </div>"
   );
+}
+
+function maxbet() {
+  document.getElementById("mybet").value = mydollars;
+  message.innerHTML = "Bet changed to $" + mydollars;
+}
+
+function cardAction(a) {
+  console.log(a);
+  switch (a) {
+    case "hit":
+      playucard(); // add new card to players hand
+      break;
+    case "hold":
+      playend(); //Playout and calculate
+      break;
+    case "double":
+      let betvalue = parseInt(document.getElementById("mybet").value);
+      if (mydollars - betvalue < 0) {
+        betvalue = betvalue + mydollars;
+        mydollars = 0;
+      } else {
+        mydollars = mydollars - betvalue;
+        betvalue = betvalue * 2;
+      }
+      document.getElementById("dollars").innerHTML = mydollars;
+      document.getElementById("mybet").value = betvalue;
+      // double current bet, remove value from mydollars
+      playucard(); // add new card to players hand
+      playend(); //Playout and calculate
+      break;
+    case "split":
+      splitCard(); //Split cards into two hands
+      break;
+    default:
+      console.log("done");
+      playend(); //Playout and calculate
+  }
+}
+
+function playucard() {
+  playerCard.push(cards[cardCount]);
+  playerHolder.innerHTML += cardOutput(cardCount, playerCard.length - 1);
+  redeal();
+  let rValu = checktotal(playerCard);
+  pValue.innerHTML = rValu;
+  if (rValu > 21) {
+    message.innerHTML = "Busted!";
+    playend();
+  }
+}
+
+function playend() {
+  endplay = true;
+  document.getElementById("cover").style.display = "none";
+  document.getElementById("myactions").style.display = "none";
+  document.getElementById("btndeal").style.display = "block";
+  document.getElementById("mybet").disabled = false;
+  document.getElementById("maxbet").disabled = false;
+  message.innerHTML = "Game Over<br>";
+  let payoutJack = 1;
+
+  let dealervalue = checktotal(dealerCard);
+  pValue.innerHTML = dealervalue;
+
+  while (dealervalue < 17) {
+    dealerCard.push(cards[cardCount]);
+    dealerHolder.innerHTML += cardOutput(cardCount, dealerCard.length - 1);
+    redeal();
+    dealervalue = checktotal(dealerCard);
+    dValue.innerHTML = dealervalue;
+  }
+
+  // Who won?
+  let playervalue = checktotal(playerCard);
+  if (playervalue == 21 && playerCard.length == 2) {
+    message.innerHTML = "Player Blackjack";
+    payoutJack = 1.5;
+  }
+
+  let betvalue = parseInt(document.getElementById("mybet").value) * payoutJack;
+
+  if (
+    (playervalue < 22 && playervalue > dealervalue) ||
+    (dealervalue > 21 && playervalue < 22)
+  ) {
+    message.innerHTML +=
+      '<span style="color: green;">You WIN! You won $' + betvalue + "</span>";
+    mydollars = mydollars + betvalue * 2;
+  } else if (playervalue > 21) {
+    message.innerHTML +=
+      '<span style="color: red;">Dealer Wins! You lost $' +
+      betvalue +
+      "</span>";
+  } else if (playervalue == dealervalue) {
+    message.innerHTML += '<span style="color: blue;">Push</span>';
+    mydollars = mydollars + betvalue;
+  } else {
+    message.innerHTML +=
+      '<span style="color: red;">Dealer Wins! You lost $' +
+      betvalue +
+      "</span>";
+    pValue.innerHTML = playervalue;
+    dollarValue.innerHTML = mydollars;
+  }
+}
+
+function checktotal(arr) {
+  let rValue = 0;
+  let aceAdjust = false;
+  for (let i in arr) {
+    if (arr[i].cardnum === "A" && !aceAdjust) {
+      aceAdjust = true;
+      rValue = rValue + 10;
+    }
+    rValue = rValue + arr[i].cardvalue;
+  }
+  if (aceAdjust && rValue > 21) {
+    rValue = rValue - 10;
+  }
+  return rValue;
 }
 
 function shuffleDeck(array) {
