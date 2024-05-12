c282b9275725c09c3641486948a6b29766c1608e
Gareth Hind <garethjhind@gmail.com>
2024-05-12 12:46:28 +1000

    Refactor: Convert To TypeScript

--- resources/newGame/src/scripts/card.ts
+++ resources/newGame/src/scripts/card.ts
@@ -8,4 +8,22 @@ export class Card {
     public cardnum: string,
     public cardvalue: number
   ) {}
+
+  static cardOutput(card: Card, index: number): string {
+    const hpos = index > 0 ? index * 60 + 0 : 0;
+
+    return `<div class="card ${card.icon}" style="left: ${hpos}px;">
+    <div class="card__top">
+        <div class="card__corner suit">
+        ${card.cardnum}
+        </div>
+    </div>
+    <div class="card__content suit"></div>
+    <div class="card__bottom">
+        <div class="card__corner suit">
+        ${card.cardnum}
+        </div>
+    </div>
+    </div>`;
+  }
 }
